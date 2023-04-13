ca74a629c48ecf8c6d120df7f14a6f5e43dad6e6
BrenoMorim <brenomorim13@gmail.com>
2023-04-13 07:16:26 -0300

    Including title of the page

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html lang='en-us'>
       <Head>
-        <link rel='icon' href="/favicon.ico" />
+        <link rel='icon' href="favicon.ico" />
         <meta name="description" content="My custom portfolio, containing my contact information, technologies I work with and also my projects"/>
+        <title>Breno Morim's Portfolio</title>
       </Head>
       <body>
         <Main />
